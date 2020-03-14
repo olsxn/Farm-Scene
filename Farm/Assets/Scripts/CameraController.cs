0e1808333817d5254d500ee49cc64631f6b5d330
Halil Olsen <olsenhalil@gmail.com>
2020-03-14 15:38:44 +0000

    1

---
+++ Farm/Assets/Scripts/CameraController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform pivot;
+
+    public Vector3 offset;
+
+    public float rotateSpeed;
+    public float maxViewAngle;
+    public float minViewAngle;
+
+    public bool invertY;
+    public bool useOffsetValues;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // get rid of cursor when playing
+        Cursor.lockState = CursorLockMode.Locked;
+
+        if(useOffsetValues)
+        {
+            // keeps camera distance from player
+            offset = target.position - transform.position;  
+        }
+
+        // pivot is moved to player and made a child of the player
+        pivot.position = target.position;
+        pivot.parent = target.transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // get x pos of mouse and rotate target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+        // get y pos of mouse and rotate pivot
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        if (invertY)
+        {
+            pivot.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            pivot.Rotate(-vertical, 0, 0);
+        }
+
+        // add limits to how far up and down camera can go
+        if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+        {
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+        if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+        {
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+        // move the camera based on the current rotation of the target and the orig offset
+        float desiredY = target.eulerAngles.y;
+        float desiredX = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(desiredX, desiredY, 0);
+        transform.position = target.position - (rotation * offset);
+
+        // if camera is below player
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+        }
+
+        // transform.position = target.position - offset; // camera moves when player moves
+        // lock camera to player
+        transform.LookAt(target); 
+    }
+}
---
+++ Farm/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Rigidbody rigidBody;
+    public float jumpForce;
+    public float moveSpeed;
+    public float gravityScale;
+    private float xOrigin;
+    private float zOrigin;
+    private float height;
+    public float respawnLimit;
+    public float fallMulti = 2.5f;
+    public float lowJumpMulti = 2f;
+
+    public CharacterController controller;
+
+    private Vector3 moveDirection;
+
+    private void Awake()
+    {
+        // startpoint
+        xOrigin = transform.position.x;
+        zOrigin = transform.position.z;
+        height = transform.position.y;
+        // checking height is fine
+        if (height < 0)
+        {
+            print("FALLPREVENT: Height is below zero! Setting secure height.");
+            height += 3;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        //rigidBody = GetComponent<Rigidbody>(); REPLACED BY CHARACTERCONTROLLER
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get input for movement, store y vector to fix jumping
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        // normalize so that speed doesn't double when moving diagonally
+        moveDirection = moveDirection.normalized * moveSpeed; 
+        moveDirection.y = yStore;
+
+        //get input for jumping, if player is touching the ground
+        if (controller.isGrounded)
+        {
+            moveDirection.y = 0f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
+
+        // add gravity to player, Time.deltaTime smooths it out for any user
+        moveDirection.y += (Physics.gravity.y * gravityScale) * Time.deltaTime;   
+        controller.Move(moveDirection * Time.deltaTime);
+
+        // player respawns to the starting point if they fall off edge (go past respawnLimit)
+        if (transform.position.y < respawnLimit)
+        {
+            transform.position = new Vector3(xOrigin, height, zOrigin);
+        }
+    }
+}
