809ca0298be52b68e638ddce6eca5f594eabf5de
Halil Olsen <olsenhalil@gmail.com>
2020-03-23 17:11:35 +0000

    1.1
    Animation controller script added

---
+++ Farm/Assets/Scripts/AnimControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimControl : MonoBehaviour
+{
+
+    public Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            anim.Play("jumping");
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            anim.Play("running");
+        }
+            
+        //anim.Play("resting");
+    }
+}
